6281c7f9b1be361b78b8c598b21bd6c27a8f91d8
Kiyogoat <guest7mode@gmail.com>
2026-02-19 21:37:14 +0530

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,21 @@
+import { DiscordSDK } from '@discord/embedded-app-sdk';
 
+const discordSdk = new DiscordSDK(import.meta.env.VITE_CLIENT_ID);
+
+async function init() {
+  await discordSdk.ready();
+  discordSdk.subscribe('READY', async () => {
+    await discordSdk.commands.setActivity({
+      activity: {
+        type: 0,
+        details: '🖤 Black Souls II',
+        state: 'The mystery, woven beyond the night sky...',
+        assets: {
+          large_image: 'Valentine3',
+          large_text: "Won't you look comical?"
+        }
+      }
+    });
+  });
+}
+init();
