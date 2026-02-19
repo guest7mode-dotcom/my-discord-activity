485f9351a82d7c10a66e9fc18d93f10f43d14338
Kiyogoat <guest7mode@gmail.com>
2026-02-19 21:32:45 +0530

    Delete client directory

--- client/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import rocketLogo from '/Valentine3.png'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
-`;
